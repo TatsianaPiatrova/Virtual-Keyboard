f82adc870553a7c4c6d790931a74c4062f4a9e2a
Tatsiana <tania.petrova@mail.ru>
2022-05-06 10:55:22 +0300

    finish landing

---
+++ script.js
@@ -0,0 +1,18 @@
+let keys = document.querySelectorAll('.keys');
+let space = document.querySelector('.space-key');
+let shift_right = document.querySelector('.shift-right');
+let shift_left = document.querySelector('.shift-left');
+let caps_lock = document.querySelector('.caps-key');
+let body = document.querySelector('body');
+let enter = document.querySelector('.enter-key');
+let tab = document.querySelector('.tab-key');
+let backspace = document.querySelector('.backspace-key');
+let ctrl_right = document.querySelector('.ctrl-right-key');
+let ctrl_left = document.querySelector('.ctrl-left-key');
+let alt_right = document.querySelector('.alt-right-key');
+let alt_left = document.querySelector('.alt-left-key');
+
+window.addEventListener('keydown', function(){
+    
+})
+

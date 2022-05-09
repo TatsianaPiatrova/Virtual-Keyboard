32ba8588c561076005a650aecca02d7c81868522
Tatsiana <tania.petrova@mail.ru>
2022-05-09 12:39:10 +0300

    add module js

---
+++ script/keyboard.js
@@ -0,0 +1,81 @@
+export default `    <div class="container">
+<div class="keyboard-container">
+    <div class="keyboard-keys">
+        <div class="row">
+            <div class="keys simbol">\`</div>
+            <div class="keys simbol">1</div>
+            <div class="keys simbol">2</div>
+            <div class="keys simbol">3</div>
+            <div class="keys simbol">4</div>
+            <div class="keys simbol">5</div>
+            <div class="keys simbol">6</div>
+            <div class="keys simbol">7</div>
+            <div class="keys simbol">8</div>
+            <div class="keys simbol">9</div>
+            <div class="keys simbol">0</div>
+            <div class="keys simbol">-</div>
+            <div class="keys simbol">=</div>
+            <div class="keys backspace-key">backspace</div>
+        </div>
+        <div class="row">
+            <div class="keys tab-key">Tab</div>
+            <div class="keys letter">Q</div>
+            <div class="keys letter">W</div>
+            <div class="keys letter">E</div>
+            <div class="keys letter">R</div>
+            <div class="keys letter">T</div>
+            <div class="keys letter">Y</div>
+            <div class="keys letter">U</div>
+            <div class="keys letter">I</div>
+            <div class="keys letter">O</div>
+            <div class="keys letter">P</div>
+            <div class="keys simbol">[</div>
+            <div class="keys simbol">]</div>
+            <div class="keys slash simbol">|</div>
+        </div>
+        <div class="row">
+            <div class="keys caps-key">Caps Lock</div>
+            <div class="keys letter">A</div>
+            <div class="keys letter">S</div>
+            <div class="keys letter">D</div>
+            <div class="keys letter">F</div>
+            <div class="keys letter">G</div>
+            <div class="keys letter">H</div>
+            <div class="keys letter">J</div>
+            <div class="keys letter">K</div>
+            <div class="keys letter">L</div>
+            <div class="keys simbol">;</div>
+            <div class="keys simbol">'</div>
+            <div class="keys enter-key">Enter</div>
+        </div>
+        <div class="row">
+            <div class="keys shift-left shift-key">Shift</div>
+            <div class="keys letter">Z</div>
+            <div class="keys letter">X</div>
+            <div class="keys letter">C</div>
+            <div class="keys letter">V</div>
+            <div class="keys letter">B</div>
+            <div class="keys letter">N</div>
+            <div class="keys letter">M</div>
+            <div class="keys simbol">,</div>
+            <div class="keys simbol">.</div>
+            <div class="keys simbol">/</div>
+            <div class="keys pg-up-key">&#x2191</div>
+            <div class="keys shift-right shift-key">Shift</div>
+        </div>
+        <div class="row">
+            <div class="keys ctrl-key ctrl-left-key">Ctrl</div>
+            <div class="keys win-key">Win</div>
+            <div class="keys alt-key alt-left-key">Alt</div>
+            <div class="keys space-key"></div>
+            <div class="keys alt-key alt-right-key">Alt</div>
+            <div class="keys ctrl-key ctrl-right-key">Ctrl</div>
+            <div class="keys pg-left-key">&#x2190</div>
+            <div class="keys pg-down-key">&#x2193</div>
+            <div class="keys pg-right-key">&#x2192</div>
+            <div class="keys language">En</div>
+        </div>
+    </div>
+</div>
+<textarea autofocus class="text"></textarea>
+</div>`
